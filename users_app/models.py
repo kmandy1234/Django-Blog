9c6ed83141613607278ebd7418122413178f5e2d
kmandy1234 <mandeepkumbhar.mk@gmail.com>
2022-05-20 13:17:49 +0530

    img.save() updated

--- users_app/models.py
+++ users_app/models.py
@@ -20,4 +20,5 @@ class Profile(models.Model):
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+            rgb_im = img.convert('RGB')
+            rgb_im.save(self.image.path)
